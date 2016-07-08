b6bd5d1172e2e8447dc41bbdbcd165fa5fec0bdc
hunter <huntergregal@gmail.com>
2016-07-08 09:29:10 -0400

    moar tweaks

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var e = "[[b;#44D544;]root&#64;huntergregal&#46;com]";
+var e = "[[b;#44D544;]root]&#64;huntergregal&#46;com";
 var App = {
     echo: function(text) {
         this.echo(text);
@@ -90,7 +90,16 @@ jQuery(document).ready(function($) {
      
      
            $('body').terminal(App, {
-         greetings: "[[b;#44D544;]Starting udev:\t\t[ OK ]\nMount devpts:\t\t[ OK ]\nConfigure kernel options\t\t[ OK ]\nSetting clock: "+Date.now()+"\t\t[ OK ]\nSYSTEM BOOT COMPLETE\t\t[ OK ]\n\n\nQuick Commands\n\techo\tenv\thelp\n\tid\t   ls\twhoami]\nroot&#64;huntergregal&#46;com:~# env\n[[b;#44D544;]NAME=HunterGregal\nTITLE=SecurityResearcher;Student\nBLOG=http://blog.huntergregal.com\nGITHUB=https://github.com/huntergregal\nTWITTER=@huntergregal\n_=/usr/bin/env]",
+         greetings: "[[b;#44D544;]"+
+		"Starting udev:                                 [ OK &rsqb;\n"+
+		"Mount devpts:                                  [ OK &rsqb;\n"+
+		"Configure kernel options                       [ OK &rsqb;\n"+
+		"Setting clock: "+Date.now()+"                  [ OK &rsqb;\n\n"+
+		"SYSTEM BOOT COMPLETE                           [ OK &rsqb;\n\n\n"+
+		"Quick Commands"+
+		"\techo          env          help\n"+
+		"\tid            ls           whoami]\n\n"+
+		"root&#64;huntergregal&#46;com:~# env\n[[b;#44D544;]NAME=HunterGregal\nTITLE=SecurityResearcher;Student\nBLOG=http://blog.huntergregal.com\nGITHUB=https://github.com/huntergregal\nTWITTER=@huntergregal\n_=/usr/bin/env]",
                 prompt: function(p){
             var path = '~'
             p(e + ":" + path + "# ");
